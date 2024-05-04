4eaa6f5fe7a4012c55750443a4bfce2cce716345
Torniketreader123 <128076687+Torniketreader123@users.noreply.github.com>
2024-05-04 15:25:53 +0400

    ADD navigation adn Screens

---
+++ src/navigations/DashboardStackNvigator.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {BlogScreen, PostScreen} from '../screens';
+import {Routes} from '../themes';
+
+const Stack = createStackNavigator();
+
+function DashboarStackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={Routes.blog}>
+        
+        <Stack.Screen name={Routes.blog} component={BlogScreen} />
+        <Stack.Screen name={Routes.Post} component={PostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default DashboarStackNavigator;
